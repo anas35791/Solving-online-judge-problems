3bfba9375a89557ef75e18b2c20513c39f19b0ef
anas35791 <anas200002@diit.edu.bd>
2022-01-01 18:56:27 +0600

    Add files via upload

---
+++ Beecrowd/BEGINNER/1007/main.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    scanf("%d",&c);
+    scanf("%d",&d);
+    printf("DIFERENCA = %d",a*b-c*d);
+    printf("\n");
+    return 0;
+}
