be4f1f89ed629628ea75016b6a65c61f7d886c1c
anas35791 <anas200002@diit.edu.bd>
2022-01-01 19:03:04 +0600

    Add files via upload

---
+++ Beecrowd/BEGINNER/1036/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+	double a,b,c,di,r1,r2;
+	scanf("%lf%lf%lf",&a,&b,&c);
+	di=sqrt((b*b)-(4*a*c));
+	if(a==0||((b*b)-(4*a*c))<0)
+	printf("Impossivel calcular\n");
+	else
+	{r1=(-b+di)/(2*a);
+		r2=(-b-di)/(2*a);
+		printf("R1 = %.5lf\n",r1);
+		printf("R2 = %.5lf\n",r2);
+		}
+	return 0;
+		}
