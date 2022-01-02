bbcce75728489fc1d47503d8a8c627d5d6266245
Anas Bin Abbas <anas200002@diit.edu.bd>
2022-01-02 18:40:07 +0600

    Add files via upload

---
+++ Beecrowd/BEGINNER/1048/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    double n,i,g;
+    scanf("%lf",&n);
+    if(n>=0&&n<=400)
+        {i=n+(n*.15);
+        g=i-n;
+        printf("Novo salario: %.2lf\n",i);
+        printf("Reajuste ganho: %.2lf\n",g);
+        printf("Em percentual: 15 %\n");}
+
+    else if(n>400&&n<=800)
+        {i=n+(n*(.12));
+        g=i-n;
+        printf("Novo salario: %.2lf\n",i);
+        printf("Reajuste ganho: %.2lf\n",g);
+        printf("Em percentual: 12 %\n");}
+
+    else if(n>800&&n<=1200)
+        {i=n+(n*(.1));
+        g=i-n;
+        printf("Novo salario: %.2lf\n",i);
+        printf("Reajuste ganho: %.2lf\n",g);
+        printf("Em percentual: 10 %\n");}
+    else if(n>1200&&n<=2000)
+        {i=n+(n*(.07));
+        g=i-n;
+        printf("Novo salario: %.2lf\n",i);
+        printf("Reajuste ganho: %.2lf\n",g);
+        printf("Em percentual: 7 %\n");}
+    else
+    {i=n+(n*(.04));
+        g=i-n;
+        printf("Novo salario: %.2lf\n",i);
+        printf("Reajuste ganho: %.2lf\n",g);
+        printf("Em percentual: 4 %\n");}
+    return 0;
+}
