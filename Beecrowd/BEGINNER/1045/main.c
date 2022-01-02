8dec42453b033460d8109067bc4de144bcad8fac
Anas Bin Abbas <anas200002@diit.edu.bd>
2022-01-02 17:24:02 +0600

    Add files via upload

---
+++ Beecrowd/BEGINNER/1045/main.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    double a,b,c;
+    scanf("%lf%lf%lf",&a,&b,&c);
+    if(a>=b&&a>=c)
+    {
+        if(a>=b+c)
+            {printf("NAO FORMA TRIANGULO\n");}
+        else if(a*a==b*b+c*c)
+            {printf("TRIANGULO RETANGULO\n");
+             }
+        else if(a*a>b*b+c*c)
+            {printf("TRIANGULO OBTUSANGULO\n");
+             }
+        else if(a*a<b*b+c*c)
+            {printf("TRIANGULO ACUTANGULO\n");
+             }
+    }
+    else if(b>=a&&b>=c)
+    {
+        if(b>=a+c)
+            {printf("NAO FORMA TRIANGULO\n");
+             }
+        else if(b*b==a*a+c*c)
+            {printf("TRIANGULO RETANGULO\n");
+             }
+        else if(b*b>a*a+c*c)
+            {printf("TRIANGULO OBTUSANGULO\n");
+            }
+        else if(b*b<a*a+c*c)
+            {printf("TRIANGULO ACUTANGULO\n");
+             }
+    }
+    else
+    {
+        if(c>=b+a)
+            {printf("NAO FORMA TRIANGULO\n");
+             }
+        else if(c*c==b*b+a*a)
+            {printf("TRIANGULO RETANGULO\n");
+             }
+        else if(c*c>b*b+a*a)
+            {printf("TRIANGULO OBTUSANGULO\n");
+             }
+        else if(c*c<b*b+a*a)
+            {printf("TRIANGULO ACUTANGULO\n");
+            }
+
+    }
+    if(a==b&&b==c&&a==c)
+    {
+        printf("TRIANGULO EQUILATERO\n");
+    }
+    else if(a==b||b==c||c==a)
+    {
+        printf("TRIANGULO ISOSCELES\n");
+    }
+    return 0;
+}
