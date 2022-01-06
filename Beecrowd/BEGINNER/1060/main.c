a2c36dea8fde5a53c851b507e9ad61e86d6aa0b8
Anas Bin Abbas <anas200002@diit.edu.bd>
2022-01-06 12:42:48 +0600

    Add files via upload

---
+++ Beecrowd/BEGINNER/1060/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a[5],count=0,i;
+    scanf("%d",&a);
+    if(a>0)
+    printf("%.0f valores positivos\n",count++);
+    return 0;
+}
